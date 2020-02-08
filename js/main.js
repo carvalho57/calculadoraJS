0a60eac11d920856ef2267e6a466bdf2152c66a0
carvalho57 <gabrielfeocarvalho57@gmail.com>
2020-02-08 10:27:38 -0300

    Corrigir metodo calc

--- js/main.js
+++ js/main.js
@@ -5,29 +5,7 @@
   var $digits = doc.querySelectorAll('[data-js="digit"]');
   var $operator = doc.querySelectorAll('[data-js="operator"]');
   var $buttonCalcular = doc.querySelector('[data-js="calcular"]');
-  var progress; 
 
-
-  $buttonCalcular.addEventListener('click',
-      function(event) {
-        event.preventDefault();
-        calc();
-  },false);
-          
-  $digits.forEach(function (element) {
-    element.addEventListener('click', function (event) {
-      event.preventDefault();                  
-      $input.value += this.value;            
-    },false);
-  });
-
-  $operator.forEach(function (element) {
-    element.addEventListener('click', function (event) {
-      event.preventDefault();
-      setOperation(this.value);      
-    },false);
-  })     
-  
   var operator = {
     '+': function soma(x,y) {
         return x + y;
@@ -40,7 +18,8 @@
     },
     '/': function divisao(x,y) {
         if(y === 0) {
-          return null;
+          alert('Impossível divisão por zero');
+          progress = null;
         }        
 
         return x / y;
@@ -50,6 +29,37 @@
     }
   };
 
+  var progress = {
+    value1: 0,
+    operator: null,    
+    value2: null,
+    exec: function exec() {
+        var functionMath  = chooseOperation(this.operator);
+        var result =  functionMath(this.value1, this.value2);
+        this.value1  = result; 
+        return result;         
+    }
+  };
+
+  $buttonCalcular.addEventListener('click',
+      function(event) {
+        event.preventDefault();
+        calc();
+  },false);
+          
+  $digits.forEach(function (element) {
+    element.addEventListener('click', function (event) {
+      event.preventDefault();                  
+      $input.value += this.value;         
+    },false);
+  });
+
+  $operator.forEach(function (element) {
+    element.addEventListener('click', function (event) {
+      event.preventDefault();
+      setOperation(this.value);      
+    },false);
+  })     
   
 
   function chooseOperation(symbol) {
@@ -57,33 +67,17 @@
   }
 
   function setOperation(oper) {
-      
-    if(progress !== undefined && $input.value === '') {    
-      progress.operation = chooseOperation(oper);
-        return;
+    progress.value1  = Number($input.value);
+    progress.operator = oper;   
+    $input.value = "";
+  }
+
+
+  function calc() {     
+    //Corrigir   
+    if(Number($input.value) !== Number(progress.value1)) {
+        progress.value2 = Number($input.value)
     }
-
-    var progress = {
-      value1: Number($input.value),
-      operator: oper,
-      operation: chooseOperation(oper),
-      value2: null,
-      exec: function exec() {
-          return this.operation(value1,value2);
-      }
-    };
-
-    $input.value = '';
+      $input.value = progress.exec();
   }
-
-  function calc() {    
-    console.log(progress);
-    $input.value = progress.exec();
-  }
-
-  function reset() {
-    $input = '';
-    progress = null;
-  }
-
 })(window,document);
