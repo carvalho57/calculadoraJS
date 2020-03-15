73a701a8b5f75ecfdee10e58ed602692c896d706
carvalho57 <gabrielfeocarvalho57@gmail.com>
2020-03-14 23:54:51 -0300

    Adicionando botao CE e apagar ultimo digito

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# calculadoraJS
+Calculadora JS
+==============
+
+Uma simples calculadora feita em Javascript
+
+### Executando a aplicação
+
+1. Faça o clone deste projeto com `git clone https://github.com/carvalho57/calculadoraJS.git`
+2. Entre na pasta do projeto e abra a index.html ou suba uma servidor local
+3. Para subir um servidor HTTP local `https://developer.mozilla.org/pt-BR/docs/Learn/Common_questions/Como_configurar_um_servidor_de_testes_local`
+4. Abra o browser e visite a página `http://localhost:8000`
+
--- js/main.js
+++ js/main.js
@@ -5,42 +5,20 @@
   var $digits = doc.querySelectorAll('[data-js="digit"]');
   var $operator = doc.querySelectorAll('[data-js="operator"]');
   var $buttonCalcular = doc.querySelector('[data-js="calcular"]');
-
-  var operator = {
-    '+': function soma(x,y) {
-        return x + y;
-    },
-    '-': function subtracao(x,y) {
-        return x - y;
-    },
-    '*': function multiplicacao(x,y) {
-        return x * y;
-    },
-    '/': function divisao(x,y) {
-        if(y === 0) {
-          alert('Impossível divisão por zero');
-          progress = null;
-        }        
-
-        return x / y;
-    },
-    '%': function mod(x,y) {
-        return x % y;
+  var $remove = doc.querySelector('[data-js="remove"]');
+  var $CE = doc.querySelector('[data-js="CE"]');
+  var $welcome = doc.querySelector('[data-js="welcome"]');
+ 
+  $remove.addEventListener('click',() => {
+    $input.value = $input.value.slice(0,-1);
+    if($input.value === '') {
+      reset();
     }
-  };
 
-  var progress = {
-    value1: 0,
-    operator: null,    
-    value2: null,
-    setOperation: null,
-    exec: function exec() {
-        var functionMath  = chooseOperation(this.operator);
-        var result =  functionMath(this.value1, this.value2);
-        this.value1  = result; 
-        return result;         
-    }
-  };
+  },false);
+  $CE.addEventListener('click',reset,false);
+
+  win.addEventListener('load', handleWelcome,false);
 
   $buttonCalcular.addEventListener('click',
       function(event) {
@@ -65,8 +43,43 @@
       event.preventDefault();      
       setOperation(this.value);      
     },false);
-  })     
-  
+  }) 
+
+  var operator = {
+    '+': function sum(x,y) {
+        return x + y;
+    },
+    '-': function subtraction(x,y) {
+        return x - y;
+    },
+    '*': function multiplication(x,y) {
+        return x * y;
+    },
+    '/': function division(x,y) {
+        if(y === 0) {
+          alert('Impossível divisão por zero');
+          progress = null;
+        }        
+
+        return x / y;
+    },
+    '%': function mod(x,y) {
+        return x % y;
+    }
+  };
+
+  let progress = {
+    value1: 0,
+    operator: null,    
+    value2: null,
+    setOperation: null,
+    exec: function exec() {
+        var functionMath  = chooseOperation(this.operator);
+        var result =  functionMath(this.value1, this.value2);
+        this.value1  = result; 
+        return result;         
+    }
+  };
 
   function chooseOperation(symbol) {
       return operator[symbol];
@@ -85,8 +98,36 @@
     if(progress.setOperation) {
         progress.value2 = Number($input.value)
     }
-
     $input.value = progress.exec();
     progress.setOperation = false;
+  }
+
+  function reset() {       
+    $input.value = '';
+    progress.value1 = 0;
+    progress.operator = null;
+    progress.value2 = null;
+    progress.setOperation = null;
+  }
+
+  function handleWelcome() {
+      timer(3);
+  }
+
+  function timer(number) {
+      if(number === -1) {
+        $welcome.textContent = "Bem - Vindo a Calculadora JS";
+        setTimeout(closeWelcome,2000);          
+        return;
+      }
+      setTimeout(function() {          
+        $welcome.textContent = number;
+        timer(number - 1);          
+      },1000);      
+  }
+
+  function closeWelcome() {
+    var modal = doc.querySelector('[data-js="modal"]');
+    modal.style.display = 'none';
   }
 })(window,document);
